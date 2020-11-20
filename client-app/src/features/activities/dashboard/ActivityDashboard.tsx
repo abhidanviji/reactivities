20dfd5c18c5b6cb8ed9a2522ccacd6acd3faba33
abhidanviji <abhi_danviji@yahoo.co.in>
2020-11-20 09:00:07 -0800

    section 16

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect } from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
+import ActivityList from "./ActivityList";
+import { observer } from "mobx-react-lite";
+import { LoadingComponent } from "../../../app/layout/LoadingComponent";
+import { RootStoreContext } from "../../../app/stores/rootStore";
+
+const ActivityDashboard: React.FC = () => {
+  const rootStore = useContext(RootStoreContext);
+  const {loadActivities, loadingInitial} = rootStore.activityStore;
+
+  useEffect(() => {
+    loadActivities();
+  }, [loadActivities]);
+
+  if (loadingInitial)
+    return <LoadingComponent content="Loading activities..." />;
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList />
+      </Grid.Column>
+      <GridColumn width={6}>
+        <h2>Activity filters</h2>
+      </GridColumn>
+    </Grid>
+  );
+};
+
+export default observer( ActivityDashboard);
